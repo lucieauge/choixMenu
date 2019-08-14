5597aa8133c49064059817a6d748fe31eb4f992a
Lucie Augé <lucieauge@MacBook-Pro-de-Lucie.local>
2019-08-14 17:32:17 +0200

    Ajout commentaires pour plus de lisibilité

--- choixMenu.c
+++ choixMenu.c
@@ -17,19 +17,19 @@ int main(int argc, char *argv[])
 
 	switch (choixMenu)
 {
-	case 1:
+	case 1: // Royal Cheese 
 		printf("ça va sentir booon le fromage");
 		break;
-	case 2:
+	case 2: // Mc Deluxe
 		printf("La version deluxe d'une marque deluxe");
 		break;
-	case 3: 
+	case 3: // Mc Bacon
 		printf("On n'a pas finit de vous faire aimer la viande...");
 		break;
-	case 4:
+	case 4: // Big Mac
 		printf("tu n'en a pas marre de faire toujours le même choix ?");
 		break;
-	default :
+	default : // si rien n'est sélectionné
 		printf("Je n'ai pas compris votre voix ! Vous ne mangerez rien du tout !");
 		break;
 }
