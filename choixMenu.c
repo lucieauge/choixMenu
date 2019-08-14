810b71683b12941b94a57da7d359b45e04b60ba2
Lucie Augé <lucieauge@MacBook-Pro-de-Lucie.local>
2019-08-14 16:44:40 +0200

    Ajout programme pour choisir son menu dans master

---
+++ choixMenu.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int choixMenu; 
+	
+	printf("=== Menu ===\n");
+	printf("1. Royal Cheese\n");
+	printf("2. Mc Deluxe\n");
+	printf("3. Mc Bacon\n");
+	printf("4. Big Mac\n");
+	printf("Votre choix ?\n");
+	scanf("%d", &choixMenu);
+
+	printf("\n\n");
+
+	switch (choixMenu)
+{
+	case 1:
+		printf("ça va sentir booon le fromage");
+		break;
+	case 2:
+		printf("La version deluxe d'une marque deluxe");
+		break;
+	case 3: 
+		printf("On n'a pas finit de vous faire aimer la viande...");
+		break;
+	case 4:
+		printf("tu n'en a pas marre de faire toujours le même choix ?");
+		break;
+	default :
+		printf("Je n'ai pas compris votre voix ! Vous ne mangerez rien du tout !");
+		break;
+}
+
+	printf("\n");
+	return 0;
+}
